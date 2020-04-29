aa0f33c976cdf6068641690ac6cb6e72efee40ca
Melissa Yates <melissampeart@gmail.com>
2020-04-29 15:49:23 -0500

    added basic infrastructure for driver class

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -1,9 +1,16 @@
 package com.revature.driver;
 
+import com.revature.menu.Menu;
+import com.revature.util.FileStuff;
+import com.revature.util.Roster;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FileStuff.readAccountFile();
+		System.out.println(Roster.accountInfoList.toString());
+		System.out.println("===========");
+		Menu.startMenu();
 
 	}
 
